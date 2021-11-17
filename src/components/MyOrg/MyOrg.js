9ac5e905012dbd38bbe5490941a19d64c1043ff5
WilliamWoods <47872796+WilliamWoods@users.noreply.github.com>
2021-11-17 16:31:00 -0600

    Added functionality with buttons
    will add modals with other pr

--- src/components/MyOrg/MyOrg.js
+++ src/components/MyOrg/MyOrg.js
@@ -4,13 +4,24 @@ import { useEffect, useState } from 'react';
 import Button from "react-bootstrap/Button";
 import OrgRegistration from '../OrgRegistration/OrgRegistration';
 import JoinRequestModal from '../JoinRequestModal/JoinRequestModal';
+import Modal from "react-bootstrap/Modal";
 
 
-function dispOrg(orgInfo){
+function dispOrg(orgInfo, handleShowManageMembers, handleShowJoinRequests){
     //TODO pull out org info
     return(
         <>
             <h1>{orgInfo}</h1>
+            <div className="smallPad" onClick={handleShowManageMembers}>
+                <Button variant="customOrange">
+                    Manage Members
+                </Button>
+            </div>
+            <div className="smallPad" onClick={handleShowJoinRequests}>
+                <Button variant="customOrange">
+                    JoinRequests
+                </Button>
+            </div>
         </>
     )
 }
@@ -23,34 +34,28 @@ function noOrg(){
                 <OrgRegistration />
             </div>
             <JoinRequestModal/>
-            <div className="smallPad">
-                <Button variant="customOrange">
-                    Manage Members
-                </Button>
-            </div>
-            <div className="smallPad">
-                <Button variant="customOrange">
-                    JoinRequests
-                </Button>
-            </div>
-            
-            
         </>
     )
 }
 
-function isInOrg(orgInfo){
+function isInOrg(orgInfo, handleShowManageMembers, handleShowJoinRequests){
     if(!orgInfo){
         return noOrg()
     }
-    return dispOrg(orgInfo)
+    return dispOrg(orgInfo, handleShowManageMembers, handleShowJoinRequests)
 }
 
 export default function MyOrg({userInfo, orgInfo, orgList}){
+    const [showManageMembers, setShowManageMembers] = useState(false);
+    const [showJoinRequests, setShowJoinRequests] = useState(false);
+
+    const handleShowManageMembers = () => setShowManageMembers(true);
+    const handleShowJoinRequests = () => setShowJoinRequests(true);
+
     console.log("heheheh", {userInfo});
     return(
         <>
-            {isInOrg(orgInfo)}
+            {isInOrg(orgInfo, handleShowManageMembers, handleShowJoinRequests)}
         </>
     )
 }
