b7d91cbb4613465ac0a49dc36b0e83dd755bba2a
NOAH HANSEN <nphansen@wisc.edu>
2021-11-17 15:27:14 -0600

    added support for global context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useMemo, createContext } from 'react';
 import Main from './main'
+import {ContextGlobal} from './contexts'
 
 function App() {
+  //for the global context
+  const [token, setToken] = useState();
+  
+  const value = useMemo(
+    () => ({token, setToken}),
+    [token]
+  );
+
   return (
-    <div>
-      <Main />
-    </div>
+    <>
+      <ContextGlobal.Provider value={value}>
+        <Main />
+      </ContextGlobal.Provider>
+    </>
   );
 }
 
