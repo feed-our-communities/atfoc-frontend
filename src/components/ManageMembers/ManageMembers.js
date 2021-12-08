7f8b452f8fea4daf46e839c74000ad24874e1ce4
WilliamWoods <47872796+WilliamWoods@users.noreply.github.com>
2021-12-08 16:21:26 -0600

    added api call

--- src/components/ManageMembers/ManageMembers.js
+++ src/components/ManageMembers/ManageMembers.js
@@ -15,8 +15,8 @@ import { ContextGlobal } from '../../contexts';
     getUsers(regMembers, admins, setRegMembers, false, context.token);
     getUsers(regMembers, admins, setAdmins, true, context.token);
 
-    let regMemberCards = makeUserCards(regMembers, false);
-    let adminCards = makeUserCards(admins, true);
+    let regMemberCards = makeUserCards(regMembers, false, context.token);
+    let adminCards = makeUserCards(admins, true, context.token);
 
     return (<>
                 <Tabs defaultActiveKey="members">
@@ -67,7 +67,7 @@ import { ContextGlobal } from '../../contexts';
 
  }
 
- function makeUserCards(users, isAdmin) {
+ function makeUserCards(users, isAdmin, token) {
 
     console.log(users);
 
@@ -76,12 +76,12 @@ import { ContextGlobal } from '../../contexts';
     let adminButton = (<></>);
     if (isAdmin) {
         adminButton = (
-            <Button variant="customOrange" type="button" onClick={function() {changeAdminStatus(false);}}>
+            <Button variant="customOrange" type="button" onClick={function() {changeAdminStatus(false, token);}}>
                 Remove Admin
             </Button>);
     } else {
         adminButton = (
-            <Button variant="customOrange" type="button" onClick={function() {changeAdminStatus(true);}}>
+            <Button variant="customOrange" type="button" onClick={function() {changeAdminStatus(true, token);}}>
                 Make Admin
             </Button>);
     }
@@ -95,7 +95,7 @@ import { ContextGlobal } from '../../contexts';
                             {adminButton}
                         </div>
                         <div class="buttonPad">
-                            <Button variant="customBlue" type="button" onClick={removeMember}>
+                            <Button variant="customBlue" type="button" onClick={function() {removeMember(token)}}>
                                 Remove
                             </Button>
                         </div>
@@ -105,14 +105,12 @@ import { ContextGlobal } from '../../contexts';
     }
 
     return cards; 
- }
+ } 
 
- //TODO fill in these functions 
+ async function changeAdminStatus(newStatus, token) {
 
- async function changeAdminStatus(newStatus, user_id) {
-
-    //TODO get user_id, token
-    let token = 0;
+    //TODO get user_id
+    let user_id = 0;
 
     var myHeaders = new Headers();
     myHeaders.append("Authorization", "Token " + token);
@@ -131,8 +129,22 @@ import { ContextGlobal } from '../../contexts';
     let result = await response.json();
 }
 
-function removeMember() {
-     //currently have no info on this call
+function removeMember(token) {
 
-     //DELETE /api/identity/org/members/id
+    //TODO get user id
+    //TODO check that parameter is right with api call
+    let user_id = 0;
+
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Token" + token);
+
+    var requestOptions = {
+    method: 'DELETE',
+    headers: myHeaders,
+    redirect: 'follow'
+    };
+
+    let response = await fetch("http://localhost:8000/api/identity/org/members/?id=" + user_id, requestOptions);
+    let result = await response.json();
+
 }
