cae41dbdc785e21fe78e2777841ae87ea2261cbb
WilliamWoods <47872796+WilliamWoods@users.noreply.github.com>
2021-12-17 18:19:46 -0600

    destructured props wrong

--- src/components/ManageMembers/ManageMembers.js
+++ src/components/ManageMembers/ManageMembers.js
@@ -6,11 +6,14 @@ import { ContextGlobal } from '../../contexts';
 /**
  * @returns manage members view
  */
-export default function ManageMembers({orgId}) {
+export default function ManageMembers(orgId) {
+    //orgId = orgId.orgId;
     const context = useContext(ContextGlobal);
 
     const [regMembers, setRegMembers] = useState([]);
     const [admins, setAdmins] = useState([]);
+
+    console.log(orgId);
 
     getUsers(regMembers, admins, setRegMembers, false, context.token, orgId);
     getUsers(regMembers, admins, setAdmins, true, context.token, orgId);
